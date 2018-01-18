7e67e6371b995a127fe9b85d7a41b8d92455fb2e
mlbpro417 <jtglandorf@gmail.com>
2018-01-17 20:20:29 -0800

    Completed PseudoClassical Queue and Stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.recentlyAdded = 0;
+  this.nextToLeave = 0;
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.recentlyAdded ++;
+  this.storage[this.recentlyAdded] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.recentlyAdded > this.nextToLeave) {
+    this.nextToLeave ++;
+    var leaving = this.storage[this.nextToLeave];
+    delete this.storage[this.nextToLeave];  
+  }
+  return leaving;
+};
+
+Queue.prototype.size = function() {
+  return this.recentlyAdded - this.nextToLeave;
 };
 
 
+new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+};
+
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.count > 0) {
+    var leaving = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+  }
+
+  return leaving;
+};
+
+Stack.prototype.size = function() {
+  return this.count;
 };
 
 
+new Stack();
