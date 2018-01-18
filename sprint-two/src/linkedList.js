79ef347c46c1eed40a2dfbc00055d174ed46c686
mlbpro417 <jtglandorf@gmail.com>
2018-01-18 12:38:43 -0800

    finished addToTail method of linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // add something to the list
+    // implementing the node function to get the scope for the new node
+    var newNode = Node(value);
+    
+    // update the tail to the newest added item
+    // update list.tail
+    if (list.tail === null) {
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    // remove the item from the list
+    if (list.head !== null) {
+      var currentHead = list.head;
+      delete list.head;
+      list.head = node.next; //node.node.next ?
+      return currentHead;
+    }
+    // update the head to the next in line
+    // update list.head
+    // return the value
   };
 
   list.contains = function(target) {
+    // check each item if its our target
+    // check node.value
+    // return boolean
   };
 
   return list;
