241317e810efd1019c9289d7b39fb5711b187244
mlbpro417 <jtglandorf@gmail.com>
2018-01-17 19:30:53 -0800

    Completed Queue Functional Shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = {};
+  newInstance.recentlyAdded = 0;
+  newInstance.nextToLeave = 0;
+  newInstance.storage = {};
+  _.extend(newInstance, queueMethods);
+
+  return newInstance;  
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.recentlyAdded++;
+  this.storage[this.recentlyAdded] = value;
+};
 
+queueMethods.dequeue = function () {
+  //check if there is a queue
+  if (this.recentlyAdded - this.nextToLeave > 0) {
+    this.nextToLeave++;
+    var leaving = this.storage[this.nextToLeave];
+    delete this.storage[this.nextToLeave];
+  }
+
+  return leaving;
+};
+
+queueMethods.size = function () {
+  //newest minus oldest
+  return this.recentlyAdded - this.nextToLeave;
+};
