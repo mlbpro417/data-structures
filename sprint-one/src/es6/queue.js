e38f1a27221b8b03295d7af60628a8bc9f04ce17
mlbpro417 <jtglandorf@gmail.com>
2018-01-17 20:31:32 -0800

    finished stack and queue for es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.recentlyAdded = 0;
+    this.nextToLeave = 0;
+    this.storage = {};
   }
-
+  enqueue(value) {
+    this.recentlyAdded ++;
+    this.storage[this.recentlyAdded] = value;
+  }
+  
+  dequeue() {
+    if (this.recentlyAdded - this.nextToLeave > 0) {
+      this.nextToLeave ++;
+      var leaving = this.storage[this.nextToLeave];
+      delete this.storage[this.nextToLeave];
+    } 
+    return leaving;
+  }
+  
+  size() {
+    return this.recentlyAdded - this.nextToLeave;
+  }
 }
+
+new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,29 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+
+  push(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  pop() {
+    if (this.count > 0) {
+      var leaving = this.storage[this.count];
+      delete this.storage[this.count];
+      this.count--; 
+    }
+
+    return leaving;
+  }
+
+  size() {
+    return this.count;
   }
 
 }
+
+new Stack();
