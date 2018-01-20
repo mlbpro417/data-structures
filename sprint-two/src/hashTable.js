646cadc8c86e33ba06fcba1ff9a4796d49a9c827
mlbpro417 <jtglandorf@gmail.com>
2018-01-20 11:35:48 -0800

    Updated HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,63 @@
 
 
 var HashTable = function() {
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this._limit = 8; // length 8
+  this._storage = LimitedArray(this._limit); // [];
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // check if theres bucket that has empty [] at idx
+  if (!Array.isArray(this._storage[index])) {
+    this._storage[index] = [];
+    this._storage[index].push([k, v]);
+  } else if (Array.isArray(this._storage[index])) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      var key = this._storage[index][i][0];
+      if (key === k) {
+        this._storage[index][i][1] = v;
+      }
+    } 
+  } else {
+    this._storage[index].push([k, v]);
+  }
+  // // we only need to check the [0] index of that array for the key
+  // does the key already exist? if so, then overwrite value
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // iterate through our storage array index
+  // key will always be the [0]
+  // value will always be [1]
+  // return the value
+  for (var i = 0; i < this._storage[index].length; i++) {
+    var key = this._storage[index][i][0];
+    if (key === k) {
+      return this._storage[index][i][1];
+    }
+  } 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  for (var i = 0; i < this._storage[index].length; i++) {
+    var key = this._storage[index][i][0];
+    if (key === k) {
+      this._storage[index][i].splice(i);
+    }
+  } 
 };
 
-
+// new HashTable();
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+//    [0,1,2,3] = 4 set limit of array
+//    use hashing function to generate an index
+//    [[],1,2,3] = check to see if there is a bucket at that index; if not make a bucket
+//    [[['hack', 'reactor'], ['javi', 'enriquez']],1,2,3] = need to check if the new key exists in the bucket 
+//    use the buckets index to check if the values exist
