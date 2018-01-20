054c435b5b5eaf68f7266198e6d8e27ca8e0a642
mlbpro417 <jtglandorf@gmail.com>
2018-01-20 11:35:32 -0800

    Completed Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this.storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  1) add and remove = constant / O(1);
+  2) contains = linear / O(n);
  */
